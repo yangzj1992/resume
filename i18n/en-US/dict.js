bf4bd623904a81c169951b1096d59647ea7337e2
yangzj1992 <yangzj1992@qq.com>
2016-04-16 23:46:26 +0800

    feat: basic build

---
+++ i18n/en-US/dict.js
@@ -0,0 +1,35 @@
+module.exports = {
+    skill_type: {
+        "frontend": "Frontend Development",
+        "backend": "Backend Development",
+        "miscellaneous": "Miscellaneous"
+    },
+    education_type: {
+        "bachelor": "(Bachelor's degree)"
+    },
+    link_type: {
+        "zhihu": "Zhihu",
+        "doubanbook": "Douban",
+        "repo": "Source",
+        "pdf": "PDF"
+    },
+    section_names: {
+        "education": {
+            "ch": "Education",
+            "en": "Education"
+        },
+        "language": {
+            "ch": "Language",
+            "en": "Language"
+        },
+        "experience": {
+            "ch": "Experience",
+            "en": "Experience"
+        },
+        "skills": {
+            "ch": "Skills",
+            "en": "Skills"
+        }
+    },
+    lt_ie8_comment: "You are using IE8 or lower version of IE, which is not supported."
+};
